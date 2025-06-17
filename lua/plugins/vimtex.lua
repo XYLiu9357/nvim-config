a246f7a467ecac2dfc03b1dc0b3665543510b2d3
XYLiu9357 <flotch2005@outlook.com>
2025-06-17 16:24:55 -0500

    disable vimtex

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -1,7 +1,17 @@
-return{
-  "lervag/vimtex",
-  lazy = false,
-  init = function()
-    vim.g.vimtex_view_method = "sumatra"
-  end
+return {
+--   "lervag/vimtex",
+--   lazy = false,
+--   init = function()
+--     vim.g.vimtex_view_method = "sumatra"
+--     vim.g.vimtex_compiler_latexmk = {
+--       executable = "/mnt/d/texlive/2023/bin/windows/latexmk.exe",
+--       options = {
+--         "-shell-escape",
+--         "-verbose",
+--         "-file-line-error",
+--         "-synctex=1",
+--         "-interaction=nonstopmode",
+--       },
+--     }
+--   end
 }
