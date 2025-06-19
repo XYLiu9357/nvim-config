91c6a24582f391bb767f2cde1b02f1ad1eabf8f9
XYLiu9357 <flotch2005@outlook.com>
2025-06-19 16:36:56 -0500

    update setup

--- lua/plugins/java.lua
+++ lua/plugins/java.lua
@@ -2,11 +2,35 @@ return {
   "mfussenegger/nvim-jdtls",
   ft = { "java" },
   config = function()
-    local jdtls = require('jdtls')
+    local jdtls = require("jdtls")
+
+    local jdtls_path = vim.fn.stdpath("data") .. "/mason/packages/jdtls"
+    local launcher_jar = vim.fn.glob(jdtls_path .. "/plugins/org.eclipse.equinox.launcher_*.jar")
+    local config_path = jdtls_path .. "/config_linux"
+    local workspace_dir = vim.fn.stdpath("cache") .. "/jdtls/workspace"
+
+    local root_dir = vim.fs.dirname(vim.fs.find({ "pom.xml", "build.gradle", ".git" }, { upward = true })[1])
+
     local config = {
-      cmd = { vim.fn.expand("~/.local/share/nvim/mason/bin/jdtls") },
-      root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw', 'pom.xml' }, { upward = true })[1]),
+      cmd = {
+        "java",
+        "--enable-native-access=ALL-UNNAMED",
+        "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+        "-Dosgi.bundles.defaultStartLevel=4",
+        "-Declipse.product=org.eclipse.jdt.ls.core.product",
+        "-Dlog.protocol=true",
+        "-Dlog.level=ALL",
+        "-Xms1g",
+        "--add-modules=ALL-SYSTEM",
+        "--add-opens", "java.base/java.util=ALL-UNNAMED",
+        "--add-opens", "java.base/java.lang=ALL-UNNAMED",
+        "-jar", launcher_jar,
+        "-configuration", config_path,
+        "-data", workspace_dir,
+      },
+      root_dir = root_dir,
     }
+
     jdtls.start_or_attach(config)
-  end
+  end,
 }
