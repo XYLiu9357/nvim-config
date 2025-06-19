34d0ca9f25dff812a7e8c6659c6e2fb0f77d8da8
XYLiu9357 <flotch2005@outlook.com>
2025-06-19 16:32:41 -0500

    update setup

--- lua/plugins/java.lua
+++ lua/plugins/java.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     local jdtls = require('jdtls')
     local config = {
-      cmd = { vim.fn.expand("~/.local/share/nvim/mason/bin/jdtls" },       
+      cmd = { vim.fn.expand("~/.local/share/nvim/mason/bin/jdtls") },
       root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw', 'pom.xml' }, { upward = true })[1]),
     }
     jdtls.start_or_attach(config)
